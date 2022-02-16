5dbe38afda3a973f113fabc1a7589def9bede4c0
rogerjv28 <56400927+rogerjv28@users.noreply.github.com>
2022-02-16 10:40:19 +0100

    Creación de la clase Capitulo 3 (Roger)

---
+++ rjuve/JavaCapitulo3/src/com/sopra/javabasico/capitulo3/Capitulo3.java
@@ -0,0 +1,21 @@
+package com.sopra.javabasico.capitulo3;
+
+/*
+ * 
+ * @author Roger
+ * @version 1.0.1
+ * @description Programa que imprime "Hola Mundo!"
+ * 
+ */
+
+public class Capitulo3 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Estoy aprendiendo a programar en Java.");
+		
+//		System.out.println("Me has mandado el texto: ");
+
+	}
+
+}
