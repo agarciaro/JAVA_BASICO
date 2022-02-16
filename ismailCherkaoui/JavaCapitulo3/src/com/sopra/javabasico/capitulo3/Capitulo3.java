21a4aa9e4b93c2f4dec3ecbe6ba96f32ea2e0c3f
ismailCherkaoui <99719027+ismailCherkaoui@users.noreply.github.com>
2022-02-16 11:28:13 +0100

    Cambiar Capitulo3.java

--- ismailCherkaoui/JavaCapitulo3/src/com/sopra/javabasico/capitulo3/Capitulo3.java
+++ ismailCherkaoui/JavaCapitulo3/src/com/sopra/javabasico/capitulo3/Capitulo3.java
@@ -11,9 +11,11 @@ import java.util.Random;
 
 public class Capitulo3 {
 	
-	Random random = new Random(9);
+	private Random random = new Random(9);
 	private double numero;
 	private boolean bool;
+	
+	//Variables de clase
 	private static String cadena = "Esto es un String";
 	private static String cadena2 = "Buenas";
 	
@@ -23,8 +25,8 @@ public class Capitulo3 {
 	 * @param numero
 	 * @param bool
 	 */
-	
 	public Capitulo3(Random random, double numero, boolean bool) {
+		int a = 5; //inaccesible desde fuera del constructor
 		this.random = random;
 		this.numero = numero;
 		this.bool = bool;
@@ -44,6 +46,22 @@ public class Capitulo3 {
 	 * @param args argumentos que recibe cuando se ejecuta la clase
 	 */
 	public static void main(String[] args) {
+		Random r = new Random(5);
+		
+		Capitulo3 cap = new Capitulo3(r, 7.7, true);
+		Capitulo3 cap1 = null;
+		Capitulo3 cap2 = null;
+		
 		System.out.println("Estoy aprendiendo a programar en java");
+		System.out.println(cap.random + " " + cap.numero + " " + cap.bool + " " + Capitulo3.cadena + " " + Capitulo3.cadena2);
+		
+		try {
+			cap.finalize();
+			cap1.finalize();
+			cap2.finalize();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
