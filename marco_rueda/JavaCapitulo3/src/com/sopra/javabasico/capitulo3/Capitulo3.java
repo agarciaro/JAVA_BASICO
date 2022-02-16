c2a41a061ba8f136502fb06ebcbbb69a917adc94
Marco Rueda <marcorueda92z@gmail.com>
2022-02-16 15:32:55 +0100

    Clase Capitulo3 hecha

--- marco_rueda/JavaCapitulo3/src/com/sopra/javabasico/capitulo3/Capitulo3.java
+++ marco_rueda/JavaCapitulo3/src/com/sopra/javabasico/capitulo3/Capitulo3.java
@@ -1,5 +1,7 @@
 package com.sopra.javabasico.capitulo3;
 
+import java.util.Random;
+
 /**
  * 
  * @author marcojoseruedaaguila
@@ -8,10 +10,72 @@ package com.sopra.javabasico.capitulo3;
 
 public class Capitulo3 {
 	
-	public static void main(String[] args) {
+	
+	private int numCapitulo;
+	private char idCiudad;
+	private Random numAleatorio;
+	private boolean isTrue;
+	
+	//---VARIABLES DE CLASE-----
+	private static String nombreCapitulo;
+	private static String esString = "Esto es una string";
+	
+	
+	
+	//----CONSTRUCTORES----
+	
+	public Capitulo3 () {}
+	
+	public Capitulo3(int numCapitulo, char idCiudad, Random numAleatorio) {
+		super();
+		this.numCapitulo = numCapitulo;
+		this.idCiudad = idCiudad;
+		this.numAleatorio = numAleatorio;
+	}
+	
+	//----BLOQUES INICIALIZADORES DE INSTANCIAS-----
+	{System.out.println("Inicializador de instancia");}
+	{System.out.println(numCapitulo);}
 		
-		System.out.println("Hola mundo!");
+	
+	public void iniciarInstancia2()
+	{
+		
+		System.out.println(numCapitulo);
 		
 	}
+	
+	
+	
+
+
+
+
+	//---MÉTODO MAIN----
+	public static void main(String[] args) {
+		
+		if(args.length != 0) {
+		System.out.println("Me has mandado el texto: " + args[0]); 
+		}
+		
+		Random r = new Random();
+		System.out.println(r.nextInt(10));
+		
+		System.gc();
+
+		
+		
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
+	}
+	
+	//---OVERRIDE IMPLEMENT METHODS - FINALIZE
+	
+	
+	
 
 }
