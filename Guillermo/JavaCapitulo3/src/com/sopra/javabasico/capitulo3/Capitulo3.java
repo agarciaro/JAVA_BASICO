50568942bc51782bcd2f11fe74721a9ea8a92d45
GuilleGalvan <61503580+GuilleGalvan@users.noreply.github.com>
2022-02-16 10:56:31 +0100

    Hasta el punto 8

--- Guillermo/JavaCapitulo3/src/com/sopra/javabasico/capitulo3/Capitulo3.java
+++ Guillermo/JavaCapitulo3/src/com/sopra/javabasico/capitulo3/Capitulo3.java
@@ -1,5 +1,8 @@
 package com.sopra.javabasico.capitulo3;
 
+//import java.util.*; //Importa todo lo que hay en java.util, resulta ineficiente.
+import java.util.Random;
+
 /**
  * 
  * @author Guille
@@ -8,11 +11,32 @@ package com.sopra.javabasico.capitulo3;
 
 public class Capitulo3 {
 
+	int var1;
+	float var2;
+	Random var3;
+	static String var4;
+	static String var5 = "Esto es un String";
+
 	public static void main(String[] args) {
 		System.out.println("Estoy aprendiendo a programar Java");
 	}
-	
+
+	// Constructor
+
+	void Capitulo3() {
+		var1 = 10;
+		var2 = 1.0f;
+		
+	}
+
+	// Funciones
+
 	public void mostrarTexto(String texto) {
 		System.out.println("Me has mandado el texto: " + texto);
+	}
+
+	public void mostrarRandom() {
+		Random r = new Random();
+		System.out.println(r.nextInt(10));
 	}
 }
