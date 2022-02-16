3c9d8fb6417fd4319c422bb1fb97dc4a379790e2
agarciaro <aitorgarciarojado@gmail.com>
2022-02-16 12:26:05 +0100

    correciones ejercicio Capitulo 3

--- ismailCherkaoui/JavaCapitulo3/src/com/sopra/javabasico/capitulo3/Capitulo3.java
+++ ismailCherkaoui/JavaCapitulo3/src/com/sopra/javabasico/capitulo3/Capitulo3.java
@@ -8,44 +8,73 @@ import java.util.Random;
  * @version 1.0
  *
  */
-
 public class Capitulo3 {
-	
-	Random r = new Random();
-	private int random = r.nextInt(9);
-	private double numero;
-	private boolean bool;
-	
-	//Variables de clase
+
+	// Variables de clase
 	private static String cadena = "Esto es un String";
 	private static String cadena2 = "Buenas";
-	
+
+	private Random r;
+	private int random;
+	private double numero;
+	private boolean bool;
+
+	{
+		System.out.println(numero);
+	}
+
 	/**
 	 * Constructor de la clase Capitulo3
+	 * 
 	 * @param random
 	 * @param numero
 	 * @param bool
 	 */
 	public Capitulo3(Random random, double numero, boolean bool) {
-		int a = 5; //inaccesible desde fuera del constructor
-		this.random = random.nextInt(5);
+		int a = 5; // inaccesible desde fuera del constructor
+		this.r = random;
 		this.numero = numero;
 		this.bool = bool;
 	}
-	
-	
+
 	/**
 	 * 
 	 * @param args argumentos que recibe cuando se ejecuta la clase
 	 */
 	public static void main(String[] args) {
-		Random r = new Random();
-		
-		Capitulo3 cap = new Capitulo3(r, 7.7, true);
-		Capitulo3 cap1 = null;
+		Random rndm = new Random();
+		System.out.println(rndm.nextInt(10));
+
+		Capitulo3 cap = new Capitulo3(rndm, 7.7, true);
+		Capitulo3 cap1 = new Capitulo3(rndm, 0, false);
 		Capitulo3 cap2 = null;
-		System.out.println("Me has mandado el texto: " + args[0]);
+		if (args.length != 0) {
+			System.out.println("Me has mandado el texto: " + args[0]);
+		}
+		
+		cap1 = null;
+		System.gc();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		System.out.println("Estoy aprendiendo a programar en java");
-		System.out.println(cap.random + " " + cap.numero + " " + cap.bool + " " + Capitulo3.cadena + " " + Capitulo3.cadena2);
+		System.out.println(cap.r.nextInt(6) + " " + cap.numero + " " + cap.bool + " " + Capitulo3.cadena + " "
+				+ Capitulo3.cadena2);
 	}
+
+	{
+		System.out.println("Este bloque es un inicializador de instancia");
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		System.out.println("Instancia recolectada por GC");
+	}
+	
+	
 }
