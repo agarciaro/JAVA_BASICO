f74d8b9977e023dda52fb247fe0000e8bc793e68
ismailCherkaoui <99719027+ismailCherkaoui@users.noreply.github.com>
2022-02-16 11:36:43 +0100

    Cambios en la Capitulo3

--- ismailCherkaoui/JavaCapitulo3/src/com/sopra/javabasico/capitulo3/Capitulo3.java
+++ ismailCherkaoui/JavaCapitulo3/src/com/sopra/javabasico/capitulo3/Capitulo3.java
@@ -11,7 +11,8 @@ import java.util.Random;
 
 public class Capitulo3 {
 	
-	private Random random = new Random(9);
+	Random r = new Random();
+	private int random = r.nextInt(9);
 	private double numero;
 	private boolean bool;
 	
@@ -27,7 +28,7 @@ public class Capitulo3 {
 	 */
 	public Capitulo3(Random random, double numero, boolean bool) {
 		int a = 5; //inaccesible desde fuera del constructor
-		this.random = random;
+		this.random = random.nextInt(5);
 		this.numero = numero;
 		this.bool = bool;
 	}
@@ -35,33 +36,16 @@ public class Capitulo3 {
 	
 	/**
 	 * 
-	 * @param msg Cadena de caracteres que mostramos por consola
-	 */
-	public static void txt(String msg) {
-		System.out.println("Me has mandado el texto: " + msg);
-	}
-	
-	/**
-	 * 
 	 * @param args argumentos que recibe cuando se ejecuta la clase
 	 */
 	public static void main(String[] args) {
-		Random r = new Random(5);
+		Random r = new Random();
 		
 		Capitulo3 cap = new Capitulo3(r, 7.7, true);
 		Capitulo3 cap1 = null;
 		Capitulo3 cap2 = null;
-		
+		System.out.println("Me has mandado el texto: " + args[0]);
 		System.out.println("Estoy aprendiendo a programar en java");
 		System.out.println(cap.random + " " + cap.numero + " " + cap.bool + " " + Capitulo3.cadena + " " + Capitulo3.cadena2);
-		
-		try {
-			cap.finalize();
-			cap1.finalize();
-			cap2.finalize();
-		} catch (Throwable e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 }
