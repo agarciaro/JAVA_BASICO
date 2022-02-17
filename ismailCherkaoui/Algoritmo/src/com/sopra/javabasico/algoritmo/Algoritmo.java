f859bb65219fcb4158c1854d004d559860af22b4
ismailCherkaoui <99719027+ismailCherkaoui@users.noreply.github.com>
2022-02-17 09:24:16 +0100

    Algoritmos burbuja y binario

--- ismailCherkaoui/Algoritmo/src/com/sopra/javabasico/algoritmo/Algoritmo.java
+++ ismailCherkaoui/Algoritmo/src/com/sopra/javabasico/algoritmo/Algoritmo.java
@@ -1,12 +1,12 @@
 package com.sopra.javabasico.algoritmo;
 
-import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Algoritmo {
 	
 	public static void burbuja(int[] array) {
 		int aux;
-		for(int i = 0; i<Array.getLength(array)-1; i++) {
+		for(int i = 0; i<array.length-1; i++) {
 			if(array[i] > array[i+1]) {
 				aux = array[i];
 				array[i] = array[i+1];
@@ -20,11 +20,12 @@ public class Algoritmo {
 		int inicio = 0;
 		int fin = array.length;
 		int mid = (inicio+fin)/2;
+		boolean found = false;
 		
 		if(num > array[0] && num < array[array.length-1]) {
-			while(inicio<=fin) {
+			while(inicio<=fin && !found) {
 				if(num == array[mid]) {
-					System.out.println("El numero buscado esta en la pos " + mid);
+					found = true;
 				} else if(num < array[mid]) {
 					fin = mid-1;
 				} else if(num > array[mid]) {
@@ -33,8 +34,22 @@ public class Algoritmo {
 				
 				mid = (inicio+fin)/2;
 			}
+						
 		}
+		
+		if(found) {
+			System.out.println("El numero buscado esta en la pos " + mid);
+		} else System.out.println("EL numero no se encuentra en el array");
 	
+	}
+	
+	public static void main(String[] args) {
+		int[] array = {1,5,2,4,6,3,9,7,3,6};
+		
+		burbuja(array);
+		System.out.println(Arrays.toString(array));
+		binario(array,8);
+		
 	}
 
 }
