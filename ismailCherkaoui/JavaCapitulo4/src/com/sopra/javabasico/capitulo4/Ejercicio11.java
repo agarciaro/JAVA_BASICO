38141a01242833c1beac3d13a9372134b1a22125
ismailCherkaoui <99719027+ismailCherkaoui@users.noreply.github.com>
2022-02-17 16:48:06 +0100

    Capitulo4

--- ismailCherkaoui/JavaCapitulo4/src/com/sopra/javabasico/capitulo4/Ejercicio11.java
+++ ismailCherkaoui/JavaCapitulo4/src/com/sopra/javabasico/capitulo4/Ejercicio11.java
@@ -3,7 +3,6 @@ package com.sopra.javabasico.capitulo4;
 public class Ejercicio11 {
 	public static void main(String[] args) {
 		float a = -1.7f;
-		(a<0) ? System.out.println(a*-1) : System.out.println(a);
 		
 		a = (a<0) ? a*-1 : a;
 		
