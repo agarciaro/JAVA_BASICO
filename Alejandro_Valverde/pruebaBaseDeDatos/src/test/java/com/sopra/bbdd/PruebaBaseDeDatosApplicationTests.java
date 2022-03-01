168baebd1517d07b81fa607d307d84d247c94224
AlejandroValverde8 <60180391+AlejandroValverde8@users.noreply.github.com>
2022-03-01 13:01:55 +0100

    Primera conexión Maven-DBeaver

---
+++ Alejandro_Valverde/pruebaBaseDeDatos/src/test/java/com/sopra/bbdd/PruebaBaseDeDatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopra.bbdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaBaseDeDatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
